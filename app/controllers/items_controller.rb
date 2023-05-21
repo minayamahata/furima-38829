924ba1ef19a3ab2acfff32ee4736ee20030c0c9d
minayamahata <ka87design@gmail.com>
2023-05-21 16:26:35 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_furima, only: [:edit, :update, :destroy]
+  before_action :prevent_url, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.order('created_at DESC')
@@ -18,14 +20,38 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   def show
     @item = Item.find(params[:id])
   end
 
-  private
+    private
 
-  def item_params
-    params.require(:item).permit(:image, :name, :discription, :category_id, :condition_id, :delivery_charge_id, :prefecture_id,
-                                 :shipping_day_id, :price).merge(user_id: current_user.id)
+    def item_params
+      params.require(:item).permit(:image, :name, :discription, :category_id, :condition_id, :delivery_charge_id, :prefecture_id,
+                                  :shipping_day_id, :price).merge(user_id: current_user.id)
+    end
+
+    def set_furima
+      @item = Item.find(params[:id])
+    end
+
+    def prevent_url
+    if @item.user_id != current_user.id
+      redirect_to root_path
+    end
   end
 end
