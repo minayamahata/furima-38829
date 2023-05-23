accffd5f3c2bfc74f7eb75ca4215b832f6eb83e1
minayamahata <ka87design@gmail.com>
2023-05-23 23:19:22 +0900

    フォーム情報の保存機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,15 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
   def index
     @order_delivery_address = OrderDeliveryAddress.new
+    @item = Item.find(params[:item_id])
+    if current_user == @item.user || @item.order.present?
+      redirect_to root_path
+      end
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_delivery_address = OrderDeliveryAddress.new(order_params)
     if @order_delivery_address.valid?
       @order_delivery_address.save
