9a7a69ffce65e56264c9dda72324c82efae3e332
minayamahata <ka87design@gmail.com>
2023-05-24 23:34:58 +0900

    売却済み商品の条件分岐（編集ページ）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -53,7 +53,7 @@ class ItemsController < ApplicationController
     end
 
     def prevent_url
-    if @item.user_id != current_user.id
+    if @item.user_id != current_user.id || @item.order != nil 
       redirect_to root_path
     end
   end
